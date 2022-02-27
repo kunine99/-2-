ab97a611db254f0659681712da48cecc52e5da71
enyo <rongyy94job@gmail.com>
2022-02-28 00:17:38 +0800

    建置前台會員登入三個頁面

---
+++ front/login.php
@@ -0,0 +1,30 @@
+<fieldset>
+    <legend>會員登入</legend>
+    <!-- tr*3>td*2 -->
+    <table>
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc" id="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pw" id="pw"></td>
+        </tr>
+        <tr>
+            <td>
+                <button onclick="login()">註冊</button>
+                <button onclick="reset()">清除</button>
+            </td>
+            <td>
+                <a href="index.php?do=forget">忘記密碼</a>
+                <a href="index.php?do=reg">尚未註冊</a>
+            </td>
+        </tr>
+    </table>
+</fieldset>
+<script>
+
+
+
+    
+</script>
