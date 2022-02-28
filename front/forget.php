b4cbcd9202fe64286a4c2563aad1c737d14671b0
enyo <rongyy94job@gmail.com>
2022-02-28 15:12:10 +0800

    完成找回密碼功能

--- front/forget.php
+++ front/forget.php
@@ -2,12 +2,21 @@
     <legend>忘記密碼</legend>
     <div>請輸入信箱以查詢密碼</div>
     <div><input type="text" name="email" id="email"></div>
-    <div></div>
+    <div id="result"></div>
     <div><button onclick="find()">尋找</button></div>
 </fieldset>
 
 <script>
 
-
+function find(){
+    // 我上面email的值要傳到後台去
+    //email就等於我現在畫面上有個ide是amil的欄位
+    //把它傳過去之後我會去執行,然後回得到一個回應，這個回應就是我的結果
+    $.post("api/find_pw.php",{email:$("#email").val()},(result)=>{
+        //不管傳過來結果是如何都給我放在網頁上
+        //我準備了一個result的地方給你放 (這時這時候去上面的空白div放一個id=result)
+        $("#result").text(result)
+    })
+}
     
 </script>
