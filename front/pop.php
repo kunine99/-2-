0cb38938a1544ba2a8d7fefb68c83d3a8271eb3e
enyo <rongyy94job@gmail.com>
2022-02-28 22:10:34 +0800

    完成最新文章及人氣文章共同的部份

--- front/pop.php
+++ front/pop.php
@@ -1,1 +1,102 @@
-pop
+<fieldset>
+    <!-- 題組二考題第12項目建置人氣文章區   直接複製news.php
+    記得加第三欄人氣
+    然後因為要照人氣排序,所以從文章撈資料那裏要記得
+    加order by `goods` desc
+    之後開始做按讚
+    -->
+
+    <legend>目前位置：首頁 > 人氣文章區</legend>
+    <!-- table>tr*2>td*3 -->
+    <table>
+        <tr>
+            <td width="30%">標題</td>
+            <td width="50%">內容</td>
+            <td>人氣</td>
+        </tr>
+        <?php
+        // 先算出總共有幾篇文章
+        $total = $News->math("count", "*");
+        // 接下來告訴他五筆換一頁
+        $div = 5;
+        //算總頁數
+        $pages = ceil($total / $div);
+        // 拿到總頁數後要知道現在在哪一頁
+        //哪一頁我們是用網址頁來做,寫3元運算
+        //如果有$_GET['p']這個東西的話,我就用這頁數,不然就從第一頁開始
+        $now = $_GET['p'] ?? 1;
+        // 知道現在的頁數後就要知道我們要從哪一筆資料開始撈
+        // 比如說五筆資料就是0 5 10 15 這樣撈
+        // 比如說3筆資料就是0 3 6 9 這樣撈
+        $start = ($now - 1) * $div;
+
+        // 拿到東西後就可以去撈我所有的資料
+        //我要撈出被設定sh是1的
+        //因為我要做分頁,所以我要限制limit,文章是從start開始抓多少筆資料出來
+        $rows = $News->all(['sh' => 1], " order by `good` desc limit $start,$div");
+        // 抓到資料後把它顯示出來
+        foreach ($rows as $key => $row) {
+        ?>
+            <tr>
+                <!-- 第一個要顯示title -->
+                <td class="switch"><?= $row['title']; ?></td>
+                <!-- 第二個要顯示文章內容 
+                用mb_substr去抓它一點點的文章內容
+                從0開始抓大概20個字-->
+                <td class="switch">
+                    <div class="short"><?= mb_substr($row['text'], 0, 20); ?>...</div>
+                    <!-- 要用顯示隱藏功能 
+                td 加class switch
+                div加上不同的class方便識別 -->
+                    <div class="full" style="display: none;"><?= nl2br($row['text']); ?></div>
+                </td>
+                <td>
+                <?=$row['good'];?>個人說<img src="icon/02B03.jpg" style="width:25px">
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+    <div>
+        <?php
+        // 前符號
+        if(($now-1)>0){
+            $prev=$now-1;
+            echo "<a href='index.php?do=news&p=$prev'>";
+            echo " < ";
+            echo "</a>";  
+        }
+
+
+
+        for ($i = 1; $i <= $pages; $i++) {
+            // 放當前頁
+            //如果$now==$i的話,我的字大小24p,不然的話16px
+            $font = ($now == $i) ? '24px' : '16px';
+            // echo我們的內容,先顯示我們的當前頁
+            //我們的當前頁在index.php?do=news這個地方 和 我們p=$i
+            echo "<a href='index.php?do=news&p=$i' style='font-size:$font'>";
+            // 中間顯示我的頁碼$i
+            echo $i;
+            echo "</a>";
+        }
+
+        // 後符號,不能超過我的總頁數
+        if(($now+1)<=$pages){
+            $next=$now+1;
+            echo "<a href='index.php?do=news&p=$next'>";
+            echo " > ";
+            echo "</a>";  
+        }
+        ?>
+    </div>
+</fieldset>
+
+
+<script>
+//將on改成hovor,刪掉click,改成pop
+    $(".switch").hovor(function() {
+        $(this).parent().find(".pop").toggle()
+    })
+</script>
